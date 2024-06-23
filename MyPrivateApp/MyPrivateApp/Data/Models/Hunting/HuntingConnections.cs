060d2ef23d5280df621634c93b745275053429ca
mangalooo <magnus_leanderson@hotmail.com>
2024-06-23 20:28:09 +0200

    Smal changes

--- MyPrivateApp/MyPrivateApp/Data/Models/Hunting/HuntingConnections.cs
+++ MyPrivateApp/MyPrivateApp/Data/Models/Hunting/HuntingConnections.cs
@@ -10,14 +10,11 @@ namespace MyPrivateApp.Data.Models.Hunting
 
         [ForeignKey("HuntingInvitation")]
         public int HuntingInvitation_FK { get; set; }
-        public HuntingInvitation? HuntingInvitation { get; set; }
 
         [ForeignKey("HuntingTeamMembers")]
         public int HuntingTeamMembers_FK { get; set; }
-        public HuntingTeamMembers? HuntingTeamMembers { get; set; }
 
         [ForeignKey("HuntingGuests")]
         public int HuntingGuests_FK { get; set; }
-        public HuntingGuests? HuntingGuests { get; set; }
     }
 }
